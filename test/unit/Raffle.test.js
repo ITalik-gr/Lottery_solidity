531a86be02cbb1a0fdc78cd78494ee69f9f339b2
Vitalii Hrytsenko <vitalik.gricenko77@gmail.com>
2023-04-13 21:22:28 +0300

    add some tests

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -44,6 +44,7 @@ const { assert, expect } = require("chai");
   })
 
   describe("Check Upkeep", async function() {
+
     it("returns false if people havent sent any ETH", async function() {
       await network.provider.send("evm_increaseTime", [interval.toNumber() + 1]); // скіп часу
       await network.provider.send("evm_mine", []); // скіп блоків
@@ -63,5 +64,20 @@ const { assert, expect } = require("chai");
       assert.equal(raffleState.toString(), "1");
       assert.equal(upkeepNeeded, false);
     })
+
+    it("returns false if enough time hasn't passed", async () => {
+      await raffle.enterRaffle({ value: raffleEntranceFee })
+      await network.provider.send("evm_increaseTime", [interval.toNumber() - 5]) // use a higher number here if this test fails
+      await network.provider.request({ method: "evm_mine", params: [] })
+      const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x") // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+      assert(!upkeepNeeded)
+  })
+  it("returns true if enough time has passed, has players, eth, and is open", async () => {
+      await raffle.enterRaffle({ value: raffleEntranceFee })
+      await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+      await network.provider.request({ method: "evm_mine", params: [] })
+      const { upkeepNeeded } = await raffle.callStatic.checkUpkeep("0x") // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+      assert(upkeepNeeded)
+  })
   })
 })
